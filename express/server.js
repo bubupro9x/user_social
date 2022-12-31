64bdf0a78b68713487c95d23cee9c3e9593f4854
Duy Mai <ducduy.dev@gmail.com>
2022-12-31 16:43:48 +0700

    v1

--- express/server.js
+++ express/server.js
@@ -128,8 +128,8 @@ app.post('/login', (req, res) => {
 // });
 
 
-// app.use(bodyParser.json());
-// app.use('/.netlify/functions/server', router);  // path must route to lambda (express/server.js)
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda (express/server.js)
 
-// module.exports = app;
-// module.exports.handler = serverless(app);
+module.exports = app;
+module.exports.handler = serverless(app);
