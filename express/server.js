a4ba768be2c81905f32f38116597d0cbedd4b063
Duy Mai <ducduy.dev@gmail.com>
2022-12-31 20:14:27 +0700

    v2

--- express/server.js
+++ express/server.js
@@ -1,20 +1,21 @@
 'use strict';
 const express = require('express');
-const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient('https://hbhrnaczdeiwuurxlgqx.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiaHJuYWN6ZGVpd3V1cnhsZ3F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI0Nzc0MjcsImV4cCI6MTk4ODA1MzQyN30.BvjLI7juNfVG-B0i72Bs-p5kalWSYjGOU94DjItMWzo');
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
+
+router.get('/', (req, res) => {
+  supabase.from('users').select().then((data, _) => {
+    res.status(200).send(data)
+
+  })
+});
+
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
