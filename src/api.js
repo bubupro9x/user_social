20dbbb241b62b79326ae5370dcadd19680f68c37
Duy Mai <ducduy.dev@gmail.com>
2022-12-31 20:46:00 +0700

    v4

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/test", (req, res) => {
   res.json({
     hello: "test!"
   });
