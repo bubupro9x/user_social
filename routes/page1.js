cc7a756eb8893ff36c73e27a3500277d31b7d43e
Duy Mai <ducduy.dev@gmail.com>
2022-12-31 21:29:08 +0700

    v1

--- routes/page1.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Get Articles Page
-router.get('/', function (req, res) {
-  res.status(201).send({ message: "hello" })
-});
-
-module.exports = router;
