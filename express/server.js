39c7b42a6eb550894dc7bee7baa172bd5d616077
Duy Mai <ducduy.dev@gmail.com>
2022-12-31 19:47:41 +0700

    v1

--- express/server.js
+++ express/server.js
@@ -1,47 +1,69 @@
+// 'use strict';
+// const express = require('express');
+// const path = require('path');
+// const serverless = require('serverless-http');
+// const app = express();
+// const bodyParser = require('body-parser');
+// const router = express.Router();
+
+// //superbase
+
+// const { createClient } = require('@supabase/supabase-js');
+// const supabase = createClient('https://hbhrnaczdeiwuurxlgqx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiaHJuYWN6ZGVpd3V1cnhsZ3F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI0Nzc0MjcsImV4cCI6MTk4ODA1MzQyN30.BvjLI7juNfVG-B0i72Bs-p5kalWSYjGOU94DjItMWzo');
+
+
+// // Link to views folder.
+// let views = path.join(__dirname, '../');
+
+// // Home route.
+// router.get('/', (req, res) => {
+//   res.status(200).send({ message: "hello" })
+//   // res.sendFile('index.html', { root: views });
+// });
+
+// // Other routes.
+// // router.get('/page1', function (req, res) {
+// //   res.sendFile('page1.html', { root: views });
+// // });
+// // router.get('/page2', function (req, res) {
+// //   res.sendFile('page2.html', { root: views });
+// // });
+// // router.get('/page3', function (req, res) {
+// //   res.sendFile('page3.html', { root: views });
+// // });
+// // router.get('/page4', function (req, res) {
+// //   res.sendFile('page4.html', { root: views });
+// // });
+
+
+// app.use(bodyParser.json());
+// // app.use('/.netlify/functions/server', router);  // path must route to lambda (express/server.js)
+
+// app.get('/', (req, res) => {
+//   res.status(200).send({ message: "hello" })
+//   // res.sendFile('index.html', { root: views });
+// });
+// module.exports = app;
+// module.exports.handler = serverless(app);
+
+
 'use strict';
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+
 const router = express.Router();
-
-//superbase
-
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient('https://hbhrnaczdeiwuurxlgqx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiaHJuYWN6ZGVpd3V1cnhsZ3F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI0Nzc0MjcsImV4cCI6MTk4ODA1MzQyN30.BvjLI7juNfVG-B0i72Bs-p5kalWSYjGOU94DjItMWzo');
-
-
-// Link to views folder.
-let views = path.join(__dirname, '../');
-
-// Home route.
 router.get('/', (req, res) => {
-  res.status(200).send({ message: "hello" })
-  // res.sendFile('index.html', { root: views });
+  res.status(200).send({ message: "Hello" })
 });
-
-// Other routes.
-// router.get('/page1', function (req, res) {
-//   res.sendFile('page1.html', { root: views });
-// });
-// router.get('/page2', function (req, res) {
-//   res.sendFile('page2.html', { root: views });
-// });
-// router.get('/page3', function (req, res) {
-//   res.sendFile('page3.html', { root: views });
-// });
-// router.get('/page4', function (req, res) {
-//   res.sendFile('page4.html', { root: views });
-// });
-
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-// app.use('/.netlify/functions/server', router);  // path must route to lambda (express/server.js)
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-app.get('/', (req, res) => {
-  res.status(200).send({ message: "hello" })
-  // res.sendFile('index.html', { root: views });
-});
 module.exports = app;
 module.exports.handler = serverless(app);
