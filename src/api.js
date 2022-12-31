f69964fdff3c74efa15cf905f928079ee668ddd1
Duy Mai <ducduy.dev@gmail.com>
2022-12-31 20:44:14 +0700

    v4

--- src/api.js
+++ src/api.js
@@ -1,43 +1,16 @@
 const express = require("express");
 const serverless = require("serverless-http");
-const cors = require("cors");
+
 const app = express();
 const router = express.Router();
 
-router.get("/", cors(), (req, res, next) => {
-  res.json([
-    {
-      id: "1",
-      name: "Julio",
-      team: "Manchester",
-      slugTeam: "manchester",
-    },
-    {
-      id: "2",
-      name: "Mark ",
-      team: "Arsenal",
-      slugTeam: "arsenal",
-    },
-  ]);
+router.get("/", (req, res) => {
+  res.json({
+    hello: "test!"
+  });
 });
 
-app.use("/.netlify/functions/api", router);
+app.use('/.netlify/functions/api', router);
 
-app.use("/.netlify/functions/api/:id", function (req, res, next) {
-  res.json([
-    {
-      id: "1",
-      name: "Julio",
-      team: "Manchester",
-      slugTeam: "manchester",
-    },
-    {
-      id: "2",
-      name: "Mark ",
-      team: "Arsenal",
-      slugTeam: "arsenal",
-    },
-  ]);
-});
-
+module.exports = app;
 module.exports.handler = serverless(app);
